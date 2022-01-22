f1a577d0ce29029ec059ad302ed265fe61df5933
julian <julian@cypherstack.com>
2022-01-22 13:26:14 -0600

    clean up structure and handle back button

--- lib/pages/onboarding_view/backup_key_warning_view.dart
+++ lib/pages/onboarding_view/backup_key_warning_view.dart
@@ -29,156 +29,175 @@ class _BackupKeyWarningViewState extends State<BackupKeyWarningView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CFColors.starryNight,
-      appBar: buildOnboardingAppBar(
-        context,
-        backButtonPressed: () async {
-          // delete created wallet name and pin
-          final walletsService =
-              Provider.of<WalletsService>(context, listen: false);
-          int result = await walletsService.deleteWallet(widget.walletName);
-
-          print("delete result: $result");
-          // check if last wallet was deleted
-          if (result == 2) {
-            Navigator.pushAndRemoveUntil(
-              context,
-              CupertinoPageRoute(
-                maintainState: false,
-                builder: (_) => OnboardingView(),
+    return WillPopScope(
+      onWillPop: () async {
+        await _onBackPressed();
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: CFColors.starryNight,
+        appBar: buildOnboardingAppBar(
+          context,
+          backButtonPressed: _onBackPressed,
+        ),
+        body: buildOnboardingBody(
+          context,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 40,
               ),
-              (_) => false,
-            );
-          } else {
-            Provider.of<BitcoinService>(context, listen: false)
-                .refreshWalletData();
-
-            final nav = Navigator.of(context);
-            nav.pop();
-            nav.pop();
-          }
-        },
-      ),
-      body: buildOnboardingBody(
-        context,
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: FittedBox(
-                child: Text(
-                  "Backup Key",
-                  style: CFTextStyles.pinkHeader,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Material(
-                  color: CFColors.fog,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        SizingUtilities.circularBorderRadius),
-                    side: BorderSide(
-                      width: 1,
-                      color: CFColors.smoke,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 16,
-                      horizontal: 20,
-                    ),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        BACKUP_KEY_WARNING,
-                        style: GoogleFonts.workSans(
-                          color: CFColors.dusk,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
+              Center(
+                child: FittedBox(
+                  child: Text(
+                    "Backup Key",
+                    style: CFTextStyles.pinkHeader,
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: 16,
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    child: Checkbox(
-                      value: _checkboxIsChecked,
-                      onChanged: (newValue) {
-                        setState(() {
-                          _checkboxIsChecked = newValue;
-                        });
-                        print("checkbox clicked. New value = $newValue");
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      "I understand that if I lose my backup key, I will not be able to access my funds.",
-                      style: GoogleFonts.workSans(
-                        color: CFColors.dusk,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: 20,
-              ),
-              child: SizedBox(
-                height: 48,
-                child: GradientButton(
-                  enabled: _checkboxIsChecked,
-                  onTap: () {
-                    if (_checkboxIsChecked) {
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute(
-                          builder: (context) {
-                            return BackupKeyView();
-                          },
-                        ),
-                      );
-                    }
-                  },
-                  child: FittedBox(
-                    child: Text(
-                      "VIEW BACKUP KEY",
-                      style: GoogleFonts.workSans(
-                        color: CFColors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+              _buildWarning(),
+              _buildCheckBox(),
+              _buildViewKeyButton(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _buildCheckBox() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+        bottom: 16,
+      ),
+      child: Row(
+        children: [
+          Container(
+            child: Checkbox(
+              value: _checkboxIsChecked,
+              onChanged: (newValue) {
+                setState(() {
+                  _checkboxIsChecked = newValue;
+                });
+                print("checkbox clicked. New value = $newValue");
+              },
+            ),
+          ),
+          Expanded(
+            child: Text(
+              "I understand that if I lose my backup key, I will not be able to access my funds.",
+              style: GoogleFonts.workSans(
+                color: CFColors.dusk,
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildViewKeyButton() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+        bottom: 20,
+      ),
+      child: SizedBox(
+        height: 48,
+        child: GradientButton(
+          enabled: _checkboxIsChecked,
+          onTap: () {
+            if (_checkboxIsChecked) {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) {
+                    return BackupKeyView();
+                  },
+                ),
+              );
+            }
+          },
+          child: FittedBox(
+            child: Text(
+              "VIEW BACKUP KEY",
+              style: GoogleFonts.workSans(
+                color: CFColors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                letterSpacing: 0.5,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildWarning() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Material(
+          color: CFColors.fog,
+          shape: RoundedRectangleBorder(
+            borderRadius:
+                BorderRadius.circular(SizingUtilities.circularBorderRadius),
+            side: BorderSide(
+              width: 1,
+              color: CFColors.smoke,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 20,
+            ),
+            child: SingleChildScrollView(
+              child: Text(
+                BACKUP_KEY_WARNING,
+                style: GoogleFonts.workSans(
+                  color: CFColors.dusk,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _onBackPressed() async {
+    // delete created wallet name and pin
+    final walletsService = Provider.of<WalletsService>(context, listen: false);
+    int result = await walletsService.deleteWallet(widget.walletName);
+
+    print("delete result: $result");
+    // check if last wallet was deleted
+    if (result == 2) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        CupertinoPageRoute(
+          maintainState: false,
+          builder: (_) => OnboardingView(),
+        ),
+        (_) => false,
+      );
+    } else {
+      Provider.of<BitcoinService>(context, listen: false).refreshWalletData();
+
+      // TODO pop back to multiple wallets view or go back to naming?
+      final nav = Navigator.of(context);
+      nav.pop();
+      nav.pop();
+    }
   }
 }
