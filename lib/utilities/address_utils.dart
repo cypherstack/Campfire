3946c97e0e5e38a4f3f8ed399d007715e8942d48
julian <julian@cypherstack.com>
2022-01-29 18:26:28 -0600

    convert firo address to scripthash util function

--- lib/utilities/address_utils.dart
+++ lib/utilities/address_utils.dart
@@ -1,10 +1,38 @@
 import 'dart:convert';
 
+import 'package:crypto/crypto.dart';
+import 'package:firo_flutter/firo_flutter.dart';
+import 'package:paymint/services/coins/firo_service.dart';
+
 class AddressUtils {
   static String condenseAddress(String address) {
     return address.substring(0, 5) +
         '...' +
         address.substring(address.length - 5);
+  }
+
+  /// attempts to convert a string to a valid scripthash
+  ///
+  /// Returns the scripthash or throws an exception on invalid firo address
+  static String convertToScriptHash(String firoAddress) {
+    try {
+      print("address: $firoAddress");
+      final output = Address.addressToOutputScript(firoAddress, firoNetwork);
+      final hash = sha256.convert(output.toList(growable: false)).toString();
+
+      final chars = hash.split("");
+      final reversedPairs = <String>[];
+      // TODO find a better/faster way to do this?
+      var i = chars.length - 1;
+      while (i > 0) {
+        reversedPairs.add(chars[i - 1]);
+        reversedPairs.add(chars[i]);
+        i -= 2;
+      }
+      return reversedPairs.join("");
+    } catch (e) {
+      throw e;
+    }
   }
 
   /// parse a firo address uri
