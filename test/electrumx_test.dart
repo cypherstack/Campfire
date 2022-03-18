12c1ef925f44aef5352a596797712a1f29b29c18
julian <julian@cypherstack.com>
2022-03-18 09:22:44 -0600

    electrumx server.ping test

--- test/electrumx_test.dart
+++ test/electrumx_test.dart
@@ -112,6 +112,43 @@ void main() {
     });
   });
 
+  group("ping", () {
+    test("ping success", () async {
+      final mockClient = MockJsonRPC();
+      final command = "server.ping";
+      final jsonArgs = '[]';
+      when(
+        mockClient.request(
+            '{"jsonrpc": "2.0", "id": "some requestId","method": "$command","params": $jsonArgs}'),
+      ).thenAnswer(
+        (_) async => {"jsonrpc": "2.0", "result": null, "id": "some requestId"},
+      );
+
+      final client = ElectrumX(
+          server: "some server", port: 0, useSSL: true, client: mockClient);
+
+      final result = await client.ping(requestID: "some requestId");
+
+      expect(result, true);
+    });
+
+    test("ping throws/fails", () {
+      final mockClient = MockJsonRPC();
+      final command = "server.ping";
+      final jsonArgs = '[]';
+      when(
+        mockClient.request(
+            '{"jsonrpc": "2.0", "id": "some requestId","method": "$command","params": $jsonArgs}'),
+      ).thenThrow(Exception());
+
+      final client = ElectrumX(
+          server: "some server", port: 0, useSSL: true, client: mockClient);
+
+      expect(() => client.ping(requestID: "some requestId"),
+          throwsA(isA<Exception>()));
+    });
+  });
+
   group("getServerFeatures", () {
     test("getServerFeatures success", () async {
       final mockClient = MockJsonRPC();
