cypherstack/Campfire

commit d8227295a82ca41852e6d0208f4a369ef7904d64

fixed default electrum x node not showing up in settings and updated back warning info

lib/pages/lockscreen2.dart
@@ -85,32 +85,40 @@ class _Lockscreen2ViewState extends State<Lockscreen2View> {
       String networkName, String walletName, String walletId) async {
     FiroNetworkType firoNetworkType;
     final nodeService = Provider.of<NodeService>(context, listen: false);
-    ElectrumXNode node;
-    switch (networkName) {
-      case "main":
-        firoNetworkType = FiroNetworkType.main;
-        node = nodeService.currentNode ??
-            ElectrumXNode(
-              address: CampfireConstants.defaultIpAddress,
-              port: CampfireConstants.defaultPort,
-              name: CampfireConstants.defaultNodeName,
-              id: Uuid().v1(),
-              useSSL: CampfireConstants.defaultUseSSL,
-            );
-        break;
-      case "test":
-        firoNetworkType = FiroNetworkType.test;
-        node = nodeService.currentNode ??
-            ElectrumXNode(
-              address: CampfireConstants.defaultIpAddressTestNet,
-              port: CampfireConstants.defaultPortTestNet,
-              name: CampfireConstants.defaultNodeNameTestNet,
-              id: Uuid().v1(),
-              useSSL: CampfireConstants.defaultUseSSLTestNet,
-            );
-        break;
-      default:
-        throw Exception("Bad firo network type encountered");
+    await nodeService.reInit();
+    ElectrumXNode node = nodeService.currentNode;
+    if (node == null) {
+      switch (networkName) {
+        case "main":
+          firoNetworkType = FiroNetworkType.main;
+          node = ElectrumXNode(
+            address: CampfireConstants.defaultIpAddress,
+            port: CampfireConstants.defaultPort,
+            name: CampfireConstants.defaultNodeName,
+            id: Uuid().v1(),
+            useSSL: CampfireConstants.defaultUseSSL,
+          );
+          break;
+        case "test":
+          firoNetworkType = FiroNetworkType.test;
+          node = ElectrumXNode(
+            address: CampfireConstants.defaultIpAddressTestNet,
+            port: CampfireConstants.defaultPortTestNet,
+            name: CampfireConstants.defaultNodeNameTestNet,
+            id: Uuid().v1(),
+            useSSL: CampfireConstants.defaultUseSSLTestNet,
+          );
+          break;
+        default:
+          throw Exception("Bad firo network type encountered");
+      }
+
+      nodeService.createNode(
+        name: node.name,
+        ipAddress: node.address,
+        port: node.port.toString(),
+        useSSL: node.useSSL,
+      );
     }
     final manager = Provider.of<Manager>(context, listen: false);
     final appDir = await getApplicationDocumentsDirectory();

lib/pages/onboarding_view/create_pin_view.dart
@@ -12,6 +12,7 @@ import 'package:paymint/pages/onboarding_view/helpers/builders.dart';
 import 'package:paymint/pages/onboarding_view/restore_wallet_view.dart';
 import 'package:paymint/services/coins/firo/firo_wallet.dart';
 import 'package:paymint/services/coins/manager.dart';
+import 'package:paymint/services/node_service.dart';
 import 'package:paymint/services/wallets_service.dart';
 import 'package:paymint/utilities/biometrics.dart';
 import 'package:paymint/utilities/cfcolors.dart';
@@ -222,6 +223,9 @@ class _CreatePinViewState extends State<CreatePinView> {
                             },
                           );
 
+                          final nodeService =
+                              Provider.of<NodeService>(context, listen: false);
+                          await nodeService.reInit();
                           ElectrumXNode defaultNode;
                           switch (firoNetworkType) {
                             case FiroNetworkType.main:
@@ -247,6 +251,13 @@ class _CreatePinViewState extends State<CreatePinView> {
                               throw Exception(
                                   "Bad firo network type encountered");
                           }
+
+                          nodeService.createNode(
+                            name: defaultNode.name,
+                            ipAddress: defaultNode.address,
+                            port: defaultNode.port.toString(),
+                            useSSL: defaultNode.useSSL,
+                          );
 
                           final appDir =
                               await getApplicationDocumentsDirectory();

lib/pages/onboarding_view/restore_wallet_view.dart
@@ -19,6 +19,7 @@ import 'package:paymint/notifications/modal_popup_dialog.dart';
 import 'package:paymint/pages/onboarding_view/onboarding_view.dart';
 import 'package:paymint/services/coins/firo/firo_wallet.dart';
 import 'package:paymint/services/coins/manager.dart';
+import 'package:paymint/services/node_service.dart';
 import 'package:paymint/services/wallets_service.dart';
 import 'package:paymint/utilities/address_utils.dart';
 import 'package:paymint/utilities/cfcolors.dart';
@@ -473,6 +474,11 @@ class _RestoreWalletFormViewState extends State<RestoreWalletFormView> {
 
                 final walletName = await walletsService.currentWalletName;
                 final walletId = await walletsService.getWalletId(walletName);
+
+                final nodeService =
+                    Provider.of<NodeService>(context, listen: false);
+                await nodeService.reInit();
+
                 ElectrumXNode defaultNode;
                 switch (widget.firoNetworkType) {
                   case FiroNetworkType.main:
@@ -496,6 +502,14 @@ class _RestoreWalletFormViewState extends State<RestoreWalletFormView> {
                   default:
                     throw Exception("Bad firo network type encountered");
                 }
+
+                nodeService.createNode(
+                  name: defaultNode.name,
+                  ipAddress: defaultNode.address,
+                  port: defaultNode.port.toString(),
+                  useSSL: defaultNode.useSSL,
+                );
+
                 final appDir = await getApplicationDocumentsDirectory();
                 final firoWallet = FiroWallet(
                   walletId: walletId,

lib/services/node_service.dart
@@ -27,12 +27,14 @@ class NodeService extends ChangeNotifier {
     refresh();
   }
 
-  refresh() {
+  void refresh({bool shouldNotifyListeners = true}) {
     _walletId = _getWalletId();
     _activeNodeName = _fetchActiveNodeName();
     _currentNode = _getCurrentNode();
     _nodes = _fetchNodes();
-    notifyListeners();
+    if (shouldNotifyListeners) {
+      notifyListeners();
+    }
   }
 
   String _fetchActiveNodeName() {
@@ -96,6 +98,7 @@ class NodeService extends ChangeNotifier {
     @required String ipAddress,
     @required String port,
     @required bool useSSL,
+    bool shouldNotifyListeners = true,
   }) {
     if (name == null || name.isEmpty) {
       throw Exception("node name must not be empty");
@@ -120,7 +123,16 @@ class NodeService extends ChangeNotifier {
       "useSSL": useSSL,
     };
 
-    wallet.put('nodes', nodes);
+    wallet.put('nodes', nodes).then((_) {
+      if (nodes.length == 1) {
+        wallet
+            .put('activeNodeName', name)
+            .then((_) => refresh(shouldNotifyListeners: shouldNotifyListeners));
+      } else {
+        refresh(shouldNotifyListeners: shouldNotifyListeners);
+      }
+    });
+
     refresh();
     return true;
   }

lib/utilities/backup_key_warning.dart
@@ -1,5 +1,5 @@
 const String BACKUP_KEY_WARNING = """
-On the next screen you will see 12 words that make up your backup key.
+On the next screen you will see 24 words that make up your backup key.
 
 Please write it down. Keep it safe and never share it with anyone. Your backup key is the only way you can access your funds if you forget PIN, lose your phone, etc.
 

commit 5bc2dfd0cd25d0013373e7af77c14b55d697fd4f

fixed bug introduced by d8227295a82ca41852e6d0208f4a369ef7904d64

lib/pages/lockscreen2.dart
@@ -83,7 +83,8 @@ class _Lockscreen2ViewState extends State<Lockscreen2View> {
 
   Future<void> logIn(
       String networkName, String walletName, String walletId) async {
-    FiroNetworkType firoNetworkType;
+    FiroNetworkType firoNetworkType =
+        FiroNetworkType.values.byName(networkName);
     final nodeService = Provider.of<NodeService>(context, listen: false);
     await nodeService.reInit();
     ElectrumXNode node = nodeService.currentNode;

lib/services/coins/firo/firo_wallet.dart
@@ -1020,7 +1020,7 @@ class FiroWallet extends CoinServiceAPI {
       return false;
     }
 
-    if (wallet.isEmpty) {
+    if (wallet.isEmpty || (await wallet.get("id")) == null) {
       // Triggers for new users automatically. Generates new wallet
       await _generateNewWallet(wallet);
       wallet.put("id", this._walletId);